4a1f17b87ff527ef655c1f3ba0109134e815ea98
malshiSathsarani-123 <malshisathsarani78@gmail.com>
2024-04-25 22:31:14 +0530

    done supplier getAll section

--- controllers/SupplierController.js
+++ controllers/SupplierController.js
@@ -1,4 +1,39 @@
+var supplierId01 = null;
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadSupplierData();
+});
 /**
+ * Load Supplier Data
+ * */
+const loadSupplierData = () => {
+    var tableBody = $('#supplierTable');
+    $.ajax({
+        method: 'GET',
+        url: "http://localhost:8080/shoe/api/v1/supplier",
+        async:true,
+        success: function (item) {
+            tableBody.empty();
+
+            item.forEach(function (item) {
+                var row = $('<tr>');
+
+                row.append($('<td>').text(item.code));
+                row.append($('<td>').text(item.name));
+                row.append($('<td>').text(item.category));
+                row.append($('<td>').text(item.address));
+                row.append($('<td>').text(item.contact1));
+                row.append($('<td>').text(item.contact2));
+                row.append($('<td>').text(item.email));
+
+                tableBody.append(row);
+            });
+        },
+        error: function (xhr, status, error) {
+            console.error("Failed to fetch item data. Status code:");
+        }
+    });
+};/**
  * Supplier Save
  * */
 $("#btnSaveSupplier").click(function () {
